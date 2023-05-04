7a97aa5a3d84fe6e9d5deaa28608eb1c786e6fe7
Guido43 <gbsdpiano@yahoo.co.uk>
2023-05-04 22:26:57 +0200

    updated JS  indentation to conform standard

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 let pokemonList = [
-    { name: 'Poliwrath', height: 1, type: [water, fighting], abilities: [damp, water - absorb, swift - swim] },
-    { name: 'Charizard', height: 1.7, type: [fire, fighting], abilities: [blaze, solar - power] },
-    { name: 'Nidoqueen', height: 1.3, type: [ground, poison], abilities: [rivalry, sheer - force] }
+    { 
+        name: 'Poliwrath', 
+        height: 1, 
+        types: ['water', 'fighting'], 
+        abilities: ['damp', 'water - absorb', 'swift - swim']
+     },
+    { 
+        name: 'Charizard', 
+        height: 1.7, 
+        types: ['fire', 'flying'], 
+        abilities: [blaze, solar - power] 
+    },
+    { 
+        name: 'Nidoqueen', 
+        height: 1.3, 
+        types: ['ground', 'poison'], 
+        abilities: ['rivalry', 'sheer - force'] 
+    }
 ];
