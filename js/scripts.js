c7254a108a4f3e67136ed036666a066b6aa293ad
Guido43 <gbsdpiano@yahoo.co.uk>
2023-05-04 22:09:44 +0200

    old JS deleted and Pokemon characters added
    A list of three Pokemon characters added with an array of objects detailing their key-values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'Mashed Potato!';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Poliwrath', height: 1, type: [water, fighting], abilities: [damp, water - absorb, swift - swim] },
+    { name: 'Charizard', height: 1.7, type: [fire, fighting], abilities: [blaze, solar - power] },
+    { name: 'Nidoqueen', height: 1.3, type: [ground, poison], abilities: [rivalry, sheer - force] }
+];
