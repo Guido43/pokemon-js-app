e0acb625301b18e73301822ebc31f8c46267c29b
Guido43 <gbsdpiano@yahoo.co.uk>
2023-05-11 13:18:59 +0200

    corrections made in forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -24,16 +24,35 @@ let pokemonList =
 //here an iife wrapping the [] as newly defined pokemonList2 with add and getAll functions
 
 let pokemonRepository = (function () {
-    let pokemonList = []
+    let pokemonList2 = [{ 
+        name: 'Nidoqueen',
+        height: 1.3, 
+        types: ['ground', 'poison'], 
+        abilities: ['rivalry', 'sheer - force']
+    },{ 
+        name: 'Poliwrath', 
+        height: 1, 
+        types: ['water', 'fighting'], 
+        abilities: ['damp', 'water - absorb', 'swift - swim']
+     },
+    { 
+        name: 'Charizard', 
+        height: 1.7, 
+        types: ['fire', 'flying'], 
+        abilities: ['blaze', 'solar - power'] 
+    },]
 
-function add(item){pokemonList.push(item);
+function add(item){pokemonList2.push(item);
 }
-function getAll () {return pokemonList;
-}
+function getAll () {return pokemonList2; 
+};
 return {add: add, getAll: getAll};
+
 })();
+pokemonRepository.add({name: 'Pikachu', height: 1.4});
 //here a forEach loop refering to the protected 'item's in the function of the iife 
-pokemonList.forEach(function(item) {document.write("<p>" + item.name + " (height " + item.height + ")</p>");});
+pokemonRepository.getAll().forEach(function(item){document.write("<p>" + item.name + " (height " + item.height + ")</p>");});
+
 
 
 
