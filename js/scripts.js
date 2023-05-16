39303afd7f08c26737207530d54bcb1a87798e35
Guido43 <gbsdpiano@yahoo.co.uk>
2023-05-17 00:24:21 +0200

    event listener added inside addList for click and log details

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,8 @@
-//here the list of Pokemon characters with their attributes as key-value objects 
-//in the array.
 
-let pokemonList = 
-    [{ 
-        name: 'Poliwrath', 
-        height: 1, 
-        types: ['water', 'fighting'], 
-        abilities: ['damp', 'water - absorb', 'swift - swim']
-     },
-    { 
-        name: 'Charizard', 
-        height: 1.7, 
-        types: ['fire', 'flying'], 
-        abilities: ['blaze', 'solar - power'] 
-    },
-    { 
-        name: 'Nidoqueen',
-        height: 1.3, 
-        types: ['ground', 'poison'], 
-        abilities: ['rivalry', 'sheer - force']
-    },
-]
-//here an iife wrapping the [] as newly defined pokemonList2 with add and getAll functions
+//here an iife wrapping the [] as newly defined repository with add and getAll functions
 
 let pokemonRepository = (function () {
-    let pokemonList2 = [{ 
+    let repository = [{ 
         name: 'Nidoqueen',
         height: 1.3, 
         types: ['ground', 'poison'], 
@@ -42,16 +20,33 @@ let pokemonRepository = (function () {
         abilities: ['blaze', 'solar - power'] 
     },]
 
-function add(item){pokemonList2.push(item);
+function add(pokemon){repository.push(pokemon);
 }
-function getAll () {return pokemonList2; 
-};
-return {add: add, getAll: getAll};
-
+function getAll () {return repository; 
+}
+function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.addEventListener('click', function () {showDetails(pokemon)});
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+}
+function showDetails(pokemon) {console.log(pokemon)};
+return {add: add, 
+    getAll: getAll,
+    addListItem: addListItem
+}
 })();
+
 pokemonRepository.add({name: 'Pikachu', height: 1.4});
+console.log(pokemonRepository.getAll());
 //here a forEach loop refering to the protected 'item's in the function of the iife 
-pokemonRepository.getAll().forEach(function(item){document.write("<p>" + item.name + " (height " + item.height + ")</p>");});
+pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+});
 
 
 
